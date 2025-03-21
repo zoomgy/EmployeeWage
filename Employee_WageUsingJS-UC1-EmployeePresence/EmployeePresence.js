ca173873d44e325fc962d1245736d08748e29224
Ayush <ayushwork0@gmail.com>
2025-03-21 23:00:10 +0530

    UC1

---
+++ Employee_WageUsingJS-UC1-EmployeePresence/EmployeePresence.js
@@ -0,0 +1,14 @@
+// UC1
+
+const IS_ABSENT= 0;
+
+let empCheck= Math.floor(Math.random()*10)%2;
+if(empCheck==IS_ABSENT){
+    console.log("Employee is absent");
+    return;
+    
+}
+else{
+    console.log("Employee is present");
+    
+}
